2c078358322676937d424788c6ad1a7ef589ab0e
Marek Cerny <me@marekcerny.com>
2018-01-10 18:29:48 +0100

    switched to chrome.storage; improve summaries

--- pan.js
+++ pan.js
@@ -100,6 +100,7 @@ var injectExec = function(codeLambda) {
   var script = document.createElement('script');
   script.textContent = '(' + codeLambda + ')();';
   (document.head||document.documentElement).appendChild(script);
+  // alert('s!');
   script.remove();
 }
 
@@ -129,6 +130,8 @@ var toTime = function (m, actm) {
 
 var toAmount = function (amount) {
   // alert(amount);
+  if (amount == 0) return '0';
+  if (!amount) return '-';
   var res = [];
   do {
     res.push(String("000" + amount%1000).slice(-3));
@@ -154,17 +157,14 @@ var pan = {
     var handle = document.getElementById('handle');
     var content = document.getElementById('content');
 
-    if (localStorage.panHidden == undefined) {
-      localStorage.setItem('panHidden', '0');
-    }
 
-    pan.div.style.right = localStorage.panHidden == '1'
-      ? kHIDDEN
-      : kDISPLAYED;
-
-    if (localStorage.selectedSection == undefined) {
-      localStorage.setItem('selectedSection', 'units');
-    }
+    chrome.storage.sync.get(['panHidden', 'selectedSection'], o => {
+      if (o.panHidden === undefined) {
+        o = {panHidden: 0, selectedSection: Object.keys(pan.tabs).shift()}
+        chrome.storage.sync.set(o);
+      }
+      pan.div.style.right = o.panHidden == 1 ? kHIDDEN : kDISPLAYED;
+    });
 
     content.innerHTML = pan.sections()
       .map( ({name: n, title: t}) => [
@@ -178,6 +178,7 @@ var pan = {
     pan.sections().forEach( section => {
       section.content = document.getElementById(section.name + '_content');
       section.header = document.getElementById(section.name + '_header');
+      // section.self = section;
     });
   },
 
@@ -191,7 +192,7 @@ var pan = {
           .toUpperCase()
           .tagAs('a', {
             href: '#' + n,
-            class: n + (n == localStorage.selectedSection ? ' active' : "")
+            class: n + (n == chrome.storage.sync.selectedSection ? ' active' : "")
           })
         )
         .join("")
@@ -200,7 +201,8 @@ var pan = {
 
     handler: function (target) {
       // alert(target);
-      const who = target.className.split(/\s+/);
+      // console.log(target);
+      const who = (target.className || " " ).split(/\s+/);
       pan.sections().forEach( ({name: section}) => {
         ['content', 'header'].forEach(part => {
           const el = document.getElementById(section + '_' + part);
@@ -208,7 +210,10 @@ var pan = {
           el.style.display = (who.indexOf(section) > -1 ? 'block' : 'none');
         });
       });
-      localStorage.selectedSection = target.className.split(/\s+/)[0];
+
+      chrome.storage.sync.set({selectedSection: target.className.split(/\s+/)[0]});
+      // console.log(target.className.split(/\s+/)[0]);
+      // chrome.storage.sync.get('selectedSection', console.log);
 
       [].slice.call(this.dom().childNodes).forEach(child => {
         child.className = child.className.split(/\s+/).filter(x=>x!='active').join(' ');
@@ -220,63 +225,125 @@ var pan = {
 
   tabs: {
 
-    units: {
-      title: 'Jednotky',
-      name: 'units',
-      defContent: () => "",
+    summary: {
+      title: 'Přehledy',
+      name: 'summary',
+
       init: function () {
-        var units = document.getElementsByClassName('unit');
-        var res = [].slice.call(units)
-          .map(x => {
-            var p = x.firstChild.title
-              .match(/\(útok ([0-9]+), obrana ([0-9]+)\)/)
-              .map(x => parseInt(x));
-            return {attack: p[1], defence: p[2], val: x};}
-           )
-          .sort((x,y) => {
-            if (x.attack+x.defence == y.attack+y.defence) {
-              return y.attack - x.attack;
-            }
-            return y.attack+y.defence - (x.attack+x.defence);
-          })
-          .map(o => {
-            const x = o.val;
-            const img = x.firstChild;
-            img.dataset.id = o.val.id.slice(1);
-            const erb = x.getElementsByClassName('F')[0].src;
-            const f = ""
-              .tagAs('img', {
-                src: x.getElementsByClassName('R')[0].src
-              })
-              .tagAs('div', {
-                class: 'erb',
-                style: 'background-image:url(' + erb + ')'
-              });
-            const name = img.title.split(/\s*(\(|-)\s*/)[0];
-            return [f
-                   ,img.outerHTML
-                   ,o.attack.toString() + '<br>' + o.defence
-                   ,name
-                   ];
-          })
-          ;
+        const self = pan.tabs.summary;
+        Object.values(self.sections).forEach( x => self.content.appendChild(x()) );
+      },
 
-        // alert(Object.keys(pan.tabs.units));
-        // alert(pan.tabs.units.content);
-        pan.tabs.units.content.innerHTML = '<br>'
-          + res
-            .map( x => x.map(tagAs('td')).join(''))
-            .map(tagAs('tr'))
-            .join('')
-            .tagAs('table', {class: 'units'})
-          + '<br>'
-            .tagAs('p', {style: 'line-height:300px;'})
-          ;
+      sections: {
+        playerName: function() {
+          let res = document.createElement('div');
+          let title = [...document.querySelectorAll('img.L2')]
+            .filter(({src:x}) => x.match(/i\/b\/(ves|c)[0-9]\w+/))
+            .map(({title: x}) => x)
+            .groupBy(x => x)
+            .map(([x,]) => x)
+            ;
 
-        [].slice.call(document.getElementsByClassName('UI')).forEach(x => {
-          x.onclick = e => injectExec('()=>unit.showUnitInfo('+e.target.dataset.id + ')');
-        });
+          let pC = x => x.tagAs('p', {style: 'text-align: center'})
+
+          // console.log(title);
+          res.innerHTML = (title || "")
+            .map(x => x.match(/\(([^\(]*)\)$/))
+            // .filter(x => x)
+            .map(x => (x && x[1]) ? x[1].tagAs('p', {class:'player_name'})
+                                  : pC('Tvoje území'))
+            .join(pC(' a '))
+            .ifEmpty(pC(say('tohle území nikomu nepatří')))
+            ;
+          return res;
+        },
+        lyingResources: function() {
+          let res = document.createElement('div');
+          res.class = 'lying_resources';
+
+          let table = {'jídla': 0, 'dřeva': 0, 'kamene': 0, 'železa': 0};
+          let images = {'jídla': 'food', 'dřeva': 'wood', 'kamene': 'stone', 'železa': 'iron'};
+          let heaps = [...document.querySelectorAll('img.P')];
+          let add2 = (a, b) => a.map( (v,i) => v + b[i] );
+
+          const sumRow = heaps
+            .map(({title: t}) => t
+              .replace('Suroviny k sebrání: ', '')
+              .split(', ')
+              .map( (x,_,b) => x.split(' ').map( y => y.trim() ) )
+            )
+            .flat()
+            .reduce( (table, [v, type]) => { table[type] += parseInt(v); return table }, table)
+            ;
+
+          res.innerHTML = Object.entries(images).map( ([key, img]) => ""
+              + toAmount(sumRow[key]).tagAs('span', {class: 'amount '+img})
+              + '&nbsp;'
+              + "".tagAs('img', {src: 'i/s/'+img+'.gif', title: 'ležícího '+key, class: 'resource'})
+            )
+            .join('  ')
+            ;
+
+          return res;
+        },
+
+        units: function() {
+          var cont = [...document.querySelectorAll('.unit')]
+            .map(x => {
+              var p = x.firstChild.title
+                .match(/\(útok ([0-9]+), obrana ([0-9]+)\)/)
+                .map(x => parseInt(x));
+              return {attack: p[1], defence: p[2], val: x};}
+             )
+            .sort((x,y) => {
+              if (x.attack+x.defence == y.attack+y.defence) {
+                return y.attack - x.attack;
+              }
+              return y.attack+y.defence - (x.attack+x.defence);
+            })
+            .map(o => {
+              const x = o.val;
+              const img = x.firstChild;
+              img.dataset.id = o.val.id.slice(1);
+              const erb = x.getElementsByClassName('F')[0].src;
+              const f = ""
+                .tagAs('img', {
+                  src: x.getElementsByClassName('R')[0].src
+                })
+                .tagAs('div', {
+                  class: 'erb',
+                  style: 'background-image:url(' + erb + ')'
+                });
+              const name = img.title.split(/\s*(\(|-)\s*/)[0];
+              return [f
+                     ,img.outerHTML
+                     ,o.attack.toString() + '<br>' + o.defence
+                     ,name
+                     ];
+            })
+            ;
+
+          let res = document.createElement('div');
+          res.class = 'units';
+          res.innerHTML = '<br>'
+            + cont
+              .map( x => x.map(tagAs('td')).join(''))
+              .map(tagAs('tr'))
+              .join('')
+              .tagAs('table', {class: 'units'})
+            + '<br>'
+              .tagAs('p', {style: 'line-height:300px;'})
+            ;
+
+          [...res.querySelectorAll('.UI')].forEach(x => {
+            x.onclick = e => injectExec('()=>unit.showUnitInfo('+e.target.dataset.id + ')');
+          });
+
+          return res;
+        }
+
       }
+
     },
 
     fights: {
@@ -284,16 +351,17 @@ var pan = {
       name: 'fights',
       init: function () {
         // alert(Object.keys(pan.tabs.fights));
-        pan.tabs.fights.content.innerHTML = Array.from(document.querySelectorAll('.unit'))
+        const fs = Array.from(document.querySelectorAll('.unit'))
           .reduce( ([m, ps], u) => {
             const {src:src, title:title, dataset:{id:id}, clientWidth:width} = u.querySelector('img.UI');
             const match = src.match(/(\w+(\d)_\d)f\.gif$/);
             if (!match) return [m, ps];
             const [,img, d] = match;
-            const coor = [u.style.left, u.style.top].map( x => parseInt(x) / 64);
-            const size = width / 64;
+            const coor = [u.style.left, u.style.top].map( x => Math.round(parseInt(x) / 64));
+            const size = Math.round(width / 64);
             m[coor.join()] = {img: img, dir: d-1, id: parseInt(id), name: title, size: size};
             // TODO : make (2x2) sizes work properly
+            // console.log(size);
             for (let i=0; i < dirs[size].length; ++i) {
               let ds = dirs[size][i][d-1];
               let p = m[[coor[0]+ds[0], coor[1]+ds[1]].join()];
@@ -303,43 +371,104 @@ var pan = {
             return [m, ps];
           }, [{}, []])
           .pop()
-          .map( ab => ab
-            // TODO sort by owner
-            .sort( ({id: x}, {id: y}) => x-y)
-            .map( (u, i) =>
-              [ u.name
-                  .replace(/\([^\)]*\)/g, "")
-                  .split('-')
-                  .map( x => x.trim())
-                  .map( (x, i) => i==0 ? x : x.tagAs('i').tagAs('a', {href: '#'}) )
-                  .reverse()
-                  .join('<br>')
-                  // .replace(/^(.*)-?(.*)$/, (_,j,a) =>  + '<br>' + j.trim())
-                  // .replace(/\s+/g, '&nbsp;')
-                  // .replace(/&nbsp;/, ' ')
-                  .tagAs('div')
-              , ""
-                  .tagAs('img', {
-                    title: u.name,
-                    src: '/i/u/'
-                          + [...u.img.split('_').slice(0,-2), (2*i+2), 3].join('_')
-                          + 'f.gif'
-                  })
-              ]
-            )
-            .map( (xs, i) => i==0 ? xs : xs.reverse())
-            .flat()
-            .map(tagAs('td'))
-            .join("")
+          .map( x => [ 'fight_' + x[0].id + '_' + x[1].id
+            , x.sort( ({id: x}, {id: y}) => x-y)
+            ]
           )
-          .map(x=>x.tagAs('tr').tagAs('table', {class: 'fights'}))
-          // .map(x=>tagAs('table class="fights"'))
-          // .map(tagAs(['tr', 'table class="fights"']))
+          ;
+
+        pan.tabs.fights.content.innerHTML = fs
+          .map( ([id, ab]) => [ id,
+            ab
+              // TODO sort by owner
+              .map( (u, i) =>
+                [ u.name
+                    .replace(/\([^\)]*\)/g, "")
+                    .split('-')
+                    .map( x => x.trim())
+                    .map( (x, i) => i==0 ? x : x.tagAs('i').tagAs('a', {href: '#'}) )
+                    .reverse()
+                    .join('<br>')
+                    // .replace(/^(.*)-?(.*)$/, (_,j,a) =>  + '<br>' + j.trim())
+                    // .replace(/\s+/g, '&nbsp;')
+                    // .replace(/&nbsp;/, ' ')
+                    .tagAs('div')
+                , ""
+                    .tagAs('img', {
+                      title: u.name,
+                      src: '/i/u/'
+                            + [...u.img.split('_').slice(0,-2), (2*i+2), 3].join('_')
+                            + 'f.gif'
+                    })
+                ]
+              )
+              .map( (xs, i) => i==0 ? xs : xs.reverse())
+              .flat()
+              .map(tagAs('td'))
+              .join("")
+            ]
+          )
+          .map(([id, x]) => x
+            .tagAs('tr')
+            .tagAs('tbody', {id: id})
+            .tagAs('table', {class: 'fights'})
+          )
           .join("")
           .ifEmpty(say('nevidím žádné souboje').tagAs('p').tagAs('center'))
           .concat('<br>' + say('může se stát, že souboj s (2x2) jednotkou nezaznamenám'))
           ;
+
+          const update = () => fs.forEach( ([id, ab]) => {
+            var row = document.createElement('tr');
+            row.class = 'fight_stats';
+            row.innerHTML = ab
+              .map( x => ""
+                .tagAs('div', {style: ''})
+                .tagAs('td', {id: id + '_' + x.id, colspan: 2})
+              )
+              .join('')
+              ;
+            document.querySelector('#'+id).appendChild(row);
+            ab.forEach( x => pan.tabs.fights.healthTracker(x.id, ([h, h_max]) =>
+                row.querySelector('#' + id + '_' + x.id).innerText = h + '/' + h_max
+              )
+            );
+          });
+
+          update();
+          setInterval(update, 60*1000);
+      },
+
+      parseAttribute: function(attr, html) {
+        const m = html
+          .split('</tr>')
+          .map( x =>
+            x.match( new RegExp( '<tr><td>' + attr + ':</td><td[^>]*>(.*)</td>'
+              .replace(/[\\\<\>]/,'\\$&'),
+            ))
+          )
+          .filter(x => x)
+          .shift()
+          ;
+        // console.log(m);
+        if (!m || !m[1]) return [-1,-1];
+        const hlth = m[1].split(' z ');
+        return [ parseInt( hlth[0] )
+               ,  eval( hlth[1].replace(/\<[^\>]+\>/g, "") )
+               ];
+      },
+
+      healthTracker: function(unitID, callback) {
+        const r = new XMLHttpRequest();
+        r.open('POST', '/unit_info.aspx?id=u' + unitID);
+        r.onload = e => {
+          const html = eval(r.responseText).filter(({attr:x})=>x=='html').map(({val:x})=>x).shift();
+          callback(pan.tabs.fights.parseAttribute('Zdraví', html));
+        }
+        r.send();
       }
+
+
     },
 
     resources: {
@@ -438,22 +567,61 @@ var pan = {
         const iframe = pan.tabs.post.content.querySelector('#posta_frame');
         // alert(pan.tabs.post.content.innerHTML);
         iframe.onload = function (e) {
-          let document = iframe.contentDocument;
-          let submit = document.getElementById('odeslat');
-          let input = document.getElementById('edit_player_name');
-          let textarea = document.getElementById('text_area');
+          let frameDoc = iframe.contentDocument;
+          let submit = frameDoc.getElementById('odeslat');
+          let input = frameDoc.getElementById('edit_player_name');
+          let textarea = frameDoc.getElementById('text_area');
 
           input.placeholder = 'adresát';
           [input, textarea, submit].forEach((e,i)=>{e.tabindex = i+10});
 
-          const style = document.createElement('style');
+          const style = frameDoc.createElement('style');
           style.innerText = '@import url("' + chrome.extension.getURL('posta.css') + '");';
-          document.body.appendChild(style);
+          frameDoc.body.appendChild(style);
+          const as = [...frameDoc.querySelectorAll('.msg_box a')]
+          as.forEach(x => x.target='top');
+          as.map(x => [x, x.href.match(/^javascript:(.*)$/)])
+            .filter(([,m]) => m && m[1])
+            .forEach(([x,m]) => {
+              x.onclick = e => injectExec('()=>{'+m[1]+'}') && false;
+              x = {href: '#javascript', target: ""};
+              x.onmouseup = e => false;
+              x.onmousedown = e => false;
+              // console.log(x);
+            });
 
           iframe.style.display = 'block';
         }
       }
     },
+
+    // geography: {
+    //   title: 'Zeměpisec',
+    //   name: 'geography',
+    //   init: function () {
+    //     var food = d=>[...d.querySelectorAll('img.P')].map(({title:x})=>x.match(/Suroviny k sebrání: (\d+) jídla/)).filter(x=>x).map(([,x])=>parseInt(x)).reduce((a,b)=>a+b,0)
+    //     var drak = d=>[...d.querySelectorAll('img')].map(({src: x})=>x.match(/\/dr([a-z])_/)).filter(f=>f&&f[1]).map(([,x])=>x).join(' ')
+    //     // var hrac = d=>[...d.querySelectorAll('img.UI')].map(({src: x}) => x.match(/.*- Larkin II \(.*/)).filter(f=>f&&f[0]).map(([x])=>x)
+    //     var hrac = d=>[...d.querySelectorAll('img.UI')].map(({title: x}) => x.match(/(.*)- Larkin II \(.*/)).filter(f=>f&&f[1]).map(([,x])=>x)
+    //     var prase = d=>[...d.querySelectorAll('img.UI')].map(x=>[x.src.match(/\/dv_/),x.id]).filter(([f])=>f&&f[0]).map(([,x]) => x)
+    //     /*
+    //     document.body.innerText.split('\n').map(x=>x.split('>')).filter(f=>f && f[2]=='orc').forEach(([x,y,t,m])=>{
+    //       const r = new XMLHttpRequest();
+    //       const u = `/main.aspx?x=${x}&y=${y}`;
+    //       r.open('GET', u, false);
+    //       r.onload = e => {
+    //           var z = new DOMParser().parseFromString(r.responseText, 'text/html');
+    //           console.log(drak(z)+prase(z)+'\t'+u+'\n')
+    //
+    //         };
+    //       console.log('> ' + u + '\t\t' + [x,y,t,m]);
+    //       r.send();
+    //     })
+    //     */
+    //     pan.tabs.geography.content.innerHTML = ""
+    //     + []
+    //   }
+    // },
 
     version: {
 
@@ -490,23 +658,26 @@ var main = function(e) {
 
     init();
     menu.load();
-    menu.handler(document.querySelector('#menu .active')
-                 || document.querySelector('#menu a') );
+    chrome.storage.sync.get('selectedSection', ({selectedSection: x}) =>
+      menu.handler(document.querySelector('#menu .' + x))
+    );
     sections().forEach( ({init: f}) => f());
 
     const handle = document.getElementById('handle');
     handle.onclick = function(e) {
-      if (localStorage.panHidden == '1') {
-        div.style.right = kDISPLAYED;
-        localStorage.panHidden = '0';
-      } else {
-        if (e.target.id != 'handle' && e.target.className != 'inner') {
-          return menu.handler(e.target);
+      chrome.storage.sync.get('panHidden', o => {
+        if (o.panHidden == 1) {
+          div.style.right = kDISPLAYED;
+          chrome.storage.sync.set({panHidden: 0});
+        } else {
+          if (e.target.id != 'handle' && e.target.className != 'inner') {
+            return menu.handler(e.target);
+          }
+          div.style.right = kHIDDEN;
+          chrome.storage.sync.set({panHidden: 1});
         }
-        div.style.right = kHIDDEN;
-        localStorage.panHidden = '1';
-      }
-    };
+      });
+    }
 
     handle.onmouseover = function(e) {
       if (div.style.right == kHIDDEN) {
